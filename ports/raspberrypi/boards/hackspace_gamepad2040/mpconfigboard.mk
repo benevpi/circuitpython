d1b61462df0da3c9b95bc5dab009ee647ae7ad25
Ben Everard <ben.everard@raspberrypi.com>
2024-02-21 11:14:57 +0000

    trying different approach

--- ports/raspberrypi/boards/hackspace_gamepad2040/mpconfigboard.mk
+++ ports/raspberrypi/boards/hackspace_gamepad2040/mpconfigboard.mk
@@ -6,7 +6,12 @@ USB_MANUFACTURER = "Raspberry Pi"
 CHIP_VARIANT = RP2040
 CHIP_FAMILY = rp2
 
+SPI_FLASH_FILESYSTEM = 1
 EXTERNAL_FLASH_DEVICES = "W25Q16JVxQ"
 
 CIRCUITPY__EVE = 1
 CIRCUITPY_PICODVI = 1
+
+# Include these Python libraries in firmware.
+#note -- stolen from 8086 commander
+FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_HID
